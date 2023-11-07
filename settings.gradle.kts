3abaeb167a27a24b94f6f7d4545e4316e8fcab27
IreneLM97 <irenelm1997@gmail.com>
2023-11-07 08:27:31 +0100

    Creación de proyecto

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PMDM_P4_MyCity_Madrid"
+include(":app")
