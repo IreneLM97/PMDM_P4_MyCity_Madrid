4cb49cdc2b0b253c6262c9320e1633ad3329f99a
IreneLM97 <irenelm1997@gmail.com>
2023-11-12 23:46:36 +0100

    Corrección para AppBar en función de la página.

--- app/src/main/java/com/example/pmdm_p4_mycity_madrid/MyCityScreen.kt
+++ app/src/main/java/com/example/pmdm_p4_mycity_madrid/MyCityScreen.kt
@@ -1,22 +1,9 @@
 package com.example.pmdm_p4_mycity_madrid
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -36,81 +23,35 @@ enum class MyCityScreen {
     PlacesList // página que muestra recomendaciones de una categoría
 }
 
-/**
- * Función para mostrar el topBar de la aplicación y el botón de volver atrás si es posible.
- */
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MyCityAppBar(
-    canNavigateBack: Boolean,
-    navigateUp: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    TopAppBar(
-        title = {
-            Text(
-                text = stringResource(R.string.city_break)
-            )
-        },
-        colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = colorResource(R.color.my_dark_purple)
-        ),
-        modifier = modifier,
-        navigationIcon = {
-            if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.back_button)
-                    )
-                }
-            }
-        }
-    )
-}
-
-/**
- * Función para navegar por las páginas de la aplicación.
- */
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun MyCityApp(
     viewModel: CityViewModel = viewModel(),
     navController: NavHostController = rememberNavController(),
     windowSize: WindowWidthSizeClass
 ) {
-    Scaffold(
-        topBar = {
-            MyCityAppBar(
-                canNavigateBack = navController.previousBackStackEntry != null,
-                navigateUp = { navController.navigateUp() }
+    NavHost(
+        navController = navController,
+        startDestination = MyCityScreen.CategoriesList.name,
+            modifier = Modifier.fillMaxSize()
+    ) {
+        composable(route = MyCityScreen.CategoriesList.name) {
+            CategoriesListScreen(
+                categories = CategoriesDataSource.getCategories(),
+                onSubcategorySelected = {
+                    viewModel.updateCurrentSubcategory(it)
+                    navController.navigate(MyCityScreen.PlacesList.name)
+                }
             )
         }
-    ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = MyCityScreen.CategoriesList.name,
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            composable(route = MyCityScreen.CategoriesList.name) {
-                CategoriesListScreen(
-                    categories = CategoriesDataSource.getCategories(),
-                    onSubcategorySelected = {
-                        viewModel.updateCurrentSubcategory(it)
-                        navController.navigate(MyCityScreen.PlacesList.name)
-                    }
-                )
-            }
 
-            composable(route = MyCityScreen.PlacesList.name) {
-                PlacesListScreen(
-                    viewModel = viewModel,
-                    windowSize = windowSize,
-                    onBackPressed = {
-                        navController.popBackStack()
-                    }
-                )
-            }
+        composable(route = MyCityScreen.PlacesList.name) {
+            PlacesListScreen(
+                viewModel = viewModel,
+                windowSize = windowSize,
+                onBackPressed = {
+                    navController.popBackStack()
+                }
+            )
         }
     }
 }
--- app/src/main/java/com/example/pmdm_p4_mycity_madrid/ui/CategoriesListScreen.kt
+++ app/src/main/java/com/example/pmdm_p4_mycity_madrid/ui/CategoriesListScreen.kt
@@ -30,6 +30,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -50,39 +54,49 @@ import com.example.pmdm_p4_mycity_madrid.R
 import com.example.pmdm_p4_mycity_madrid.data.CategoriesDataSource
 import com.example.pmdm_p4_mycity_madrid.model.Category
 import com.example.pmdm_p4_mycity_madrid.model.Subcategory
+
 // TODO COMENTAR TODO BIEN
-/**
- * Define la estructura que tendrá la pantalla que lista las categorías.
- */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoriesListScreen(
     categories: List<Category>,
     onSubcategorySelected: (Subcategory) -> Unit,
     modifier: Modifier = Modifier
 ){
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
-        modifier = modifier
-            .fillMaxSize()
-            .padding(bottom = dimensionResource(R.dimen.padding_medium)),
-    ) {
-        // Creamos una cabecera con la silueta de la ciudad
-        item {
-            CityHeader()
-        }
-        // Creamos las categorías
-        items(categories) { category ->
-            CategoryItem(
-                category = category,
-                onSubcategorySelected = onSubcategorySelected
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(R.string.city_break))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(R.color.my_dark_purple)
+                )
             )
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            modifier = modifier
+                .fillMaxSize()
+                .padding(bottom = dimensionResource(R.dimen.padding_medium)),
+            contentPadding = innerPadding
+        ) {
+            // Creamos una cabecera con la silueta de la ciudad
+            item {
+                CityHeader()
+            }
+            // Creamos las categorías
+            items(categories) { category ->
+                CategoryItem(
+                    category = category,
+                    onSubcategorySelected = onSubcategorySelected
+                )
+            }
         }
     }
 }
 
-/**
- * Define la estructura que tendrá el inicio de la página con la silueta y el nombre de la ciudad.
- */
 @Composable
 private fun CityHeader() {
     Column(
